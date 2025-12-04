78602bb4aabe6150288edf87ddc88979c0b75468
apalmaPL <apalma@product-league.com>
2025-12-04 11:54:28 +0100

    Update signicat.js

--- www/signicat.js
+++ www/signicat.js
@@ -1,19 +1,7 @@
 var exec = require("cordova/exec");
 
 module.exports = {
-  login: function (config, success, error) {
-    exec(success, error, "SignicatPlugin", "loginAppToApp", [config]);
+  login: function (success, error) {
+    exec(success, error, "SignicatPlugin", "loginAppToApp", []);
   },
-
-  getAccessToken: function (success, error) {
-    exec(success, error, "SignicatPlugin", "getAccessToken", []);
-  },
-
-  enableDeviceAuth: function (success, error) {
-    exec(success, error, "SignicatPlugin", "enableDeviceAuth", []);
-  },
-
-  disableDeviceAuth: function (success, error) {
-    exec(success, error, "SignicatPlugin", "disableDeviceAuth", []);
-  }
 };
