32a8e7bc0555bca74555d8d7d1f2ed385ef335ea
Adriano Palma <neozarp@gmail.com>
2026-02-05 11:50:15 +0000

    deleted the .m .h files

--- src/ios/AppDelegate+Signicat.h
+++
@@ -1,9 +0,0 @@
-
-#import "AppDelegate.h"
-
-
-@interface AppDelegate (SignicatPlugin)
-
-- (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray *))restorationHandler;
-
-@end
