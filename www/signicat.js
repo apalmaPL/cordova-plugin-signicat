2fc19a40e767718d5955f791f9a17ba1954f3338
psolipaPL <126564692+psolipaPL@users.noreply.github.com>
2026-02-06 10:09:39 +0000

    Update signicat.js

--- www/signicat.js
+++ www/signicat.js
@@ -1,19 +1,4 @@
 var exec = require("cordova/exec");
-/*
-module.exports = {
-  login: function (success, error) {
-    exec(success, error, "Signicat", "loginAppToApp", []);
-  },
-};
-
-window.handleOpenURL = function(url) {
-    setTimeout(function() {
-        alert(url);
-        console.log(url);
-    }, 0);
-}
-*/
-
 
 module.exports = {
   login: function (successHandler,errorHandler,issuer,clientID,redirectURI,appToAppScopes,brokerDigidAppAcs,isAppToApp) {
